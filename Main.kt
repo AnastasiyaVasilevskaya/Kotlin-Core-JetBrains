24e75d235360a26f650eb3acd13f3a378fb410f7
Anastasiya Vasilevskaya <94404108+nastyavasilevskaya@users.noreply.github.com>
2023-11-28 00:17:34 +0300

    Squares of natural numbers
    You have an integer number N. Read it from the standard input and print all squares of natural numbers that do not exceed N in ascending order.

--- Main.kt
+++ Main.kt
@@ -1,6 +1,9 @@
 fun main() {
-    val index = readln().toInt()
-    val word = readln()
-    val mess = "There isn't such an element in the given string, please fix the index!"
-    println(if (index < 0 || index >= word.length) mess else word[index])
+    val num = readln().toInt()
+    var a = 1
+    while(a * a <= num){
+        val sq = a * a
+        println (sq)
+        a++
+    }
 }
